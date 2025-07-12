766a6953196ee1fed0a3281e6636864ee4af711c
wldy4627 <wldy4627@gmail.com>
2025-07-13 02:59:58 +0900

    fix: 게시물 views.py 수정

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -28,21 +28,21 @@ def detail(request, pk):
     return render(request, 'posts/detail.html', context=context)
 
 def update(request, pk):
+    post = Post.objects.get(id=pk)
     if request.method == 'GET':
-        post = Post.objects.get(id=pk)
         form = PostForm(instance=post)
-        context = { 'form': form, 
-                    'pk': pk }
-        
-        return render(request, 'posts/post_update.html', context=context)
+        context = {
+            'form': form, 
+            'pk': pk
+        }
+        return render(request, 'posts/update.html', context=context)
     else:
-        post = Post.objects.get(id=pk)
         form = PostForm(instance=post)
         if form.is_valid():
             form.save()
-        return redirect(f'posts/detail/{pk}', pk)
+        return redirect(f'/posts/detail/{pk}')
 
 def delete(request, pk):
-    post = Post.Objects.get(id=pk)
+    post = Post.objects.get(id=pk)
     post.delete()
     return redirect('/')
