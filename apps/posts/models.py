5299e8d43576050c7ca76f5878d3f5c0ff2ff9d4
wldy4627 <wldy4627@gmail.com>
2025-07-13 02:58:17 +0900

    fix: 게시물 created_at 생성 시기 오류 수정

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
@@ -7,5 +8,10 @@ class Post(models.Model):
     region = models.CharField('지역', max_length=20)
     user = models.CharField('작성자', max_length=20)
     price = models.IntegerField('가격', default=1000)
-    created_at = models.DateTimeField('작성일', blank=True, auto_created=True, auto_now_add=True)
-    updated_at = models.DateTimeField('수정일', blank=True, auto_created=True, auto_now_add=True)
+    created_at = models.DateTimeField('작성일', auto_now_add=True)
+    updated_at = models.DateTimeField('수정일', null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk:  # 수정일 때에만 갱신
+            self.updated_at = timezone.now()
+        super().save(*args, **kwargs)
